68fd06a82042786c4540e3edccc03d379c3783f0
Jordan Castillo <jordanxcallaway@gmail.com>
2020-02-05 21:17:14 -0800

    refactored to prep for client side

--- src/app.js
+++ src/app.js
@@ -5,7 +5,9 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
-const ArticlesService = require('./articles-service')
+const articlesRouter = require('./articles/articles-router');
+const usersRouter = require('./users/users-router');
+const commentsRouter = require('./comments/comments-router');
 
 const app = express()
 
@@ -17,28 +19,14 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
-app.get('/articles', (req, res, next) => {
-  const knexInstance = req.app.get('db')
-  ArticlesService.getAllArticles(knexInstance)
-    .then(articles => {
-      res.json(articles)
-    })
-    .catch(next)
-})
+app.use('/api/articles', articlesRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/comments', commentsRouter);
 
-app.get('/articles/:article_id', (req, res, next) => {
-  const knexInstance = req.app.get('db')
-  ArticlesService.getById(knexInstance, req.params.article_id)
-    .then(article => {
-      if (!article) {
-        return res.status(404).json({
-          error: { message: 'Article doesn\'t exist' }
-        })
-      }
-      res.json(article)
-    })
-    .catch(next)
-})
+app.get('/xss', (req, res) => {
+  res.cookie('secretToken', '1234567890');
+  res.sendFile(__dirname + '/xss-example.html');
+});
 
 app.use(function errorHandler(error, req, res, next) {
   let response
